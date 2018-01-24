b8c7846f25c21f05ecf0d9ca7b6ee04707683372
Samson Weldegebriel <31634030+SamsonWeldegebriel@users.noreply.github.com>
2018-01-23 20:29:17 -0600

    Homework seven completed

--- Homework07_Express_Mongo/app.js
+++ Homework07_Express_Mongo/app.js
@@ -7,12 +7,13 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var secret = require('./routes/secret');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -24,6 +25,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/secret', secret);
+
+app.locals.plaintext = '';
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -42,5 +46,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(3000, () => console.log('app listening on port 3000!'))
 
 module.exports = app;
