cf9ea6bd9cfe21e6e76f24392b332aebb5ce4372
Samson Weldegebriel <31634030+SamsonWeldegebriel@users.noreply.github.com>
2018-02-12 08:38:51 -0600

    update

--- Homework13_Angular3/ex1/src/app/app.module.ts
+++ Homework13_Angular3/ex1/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { StudentsComponent } from './students/students.component';
+
+import {RouterModule, Routes} from '@angular/router';
+
+const ROUTES: Routes = [
+  {path: '', redirectTo: 'home', pathMatch:'full'},
+  // {path: 'home', component: AppComponent},
+  {path: 'students', component: StudentsComponent},
+  {path: '**', redirectTo: 'home'}
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
